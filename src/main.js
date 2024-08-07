5298e941ac24b391a16d7c08479f07a9f3132997
sulaiman001221 <sulaimanndlovu@gmail.com>
2024-08-07 19:12:28 +0200

    removed prevent default

--- src/main.js
+++ src/main.js
@@ -1,31 +1,35 @@
-let submit = document.querySelector('button') 
-let json_data;  
-let data  
+let submit = document.querySelector("button");
+let json_data;
+let data;
 
 let getData = () => {
-    console.log(`json ${json_data}`)
-}
+  console.log(`json ${json_data}`);
+};
 
-data = window.addEventListener('click',function(e){
-    e.preventDefault() 
+const updateDom = () => {
+  const formCard = document.querySelector(".main-form");
+  const successfulCard = document.querySelector(".successful-rsvp");
+  formCard.style.display = "none";
+  successfulCard.style.display = "block";
+};
 
-    let firstName = document.querySelector('#fname').value; 
-    let lastName = document.querySelector('#lname').value; 
-    let email = document.querySelector('#email').value; 
-    let cellphone = document.querySelector('#cellphone').value;
+const saveData = () => {
 
-     data = { 
-        firstName, 
-        lastName, 
-        email, 
-        cellphone, 
-    }     
-    json_data = JSON.stringify(data)   
+  let firstName = document.querySelector("#fname").value;
+  let lastName = document.querySelector("#lname").value;
+  let email = document.querySelector("#email").value;
+  let cellphone = document.querySelector("#cellphone").value;
 
-    getData()
+  data = {
+    firstName,
+    lastName,
+    email,
+    cellphone,
+  };
+  json_data = JSON.stringify(data);
 
-}) 
+  getData();
+  updateDom();
+};
 
-
-console.log(`jason ${json_data}`) 
-
+console.log(`jason ${json_data}`);
